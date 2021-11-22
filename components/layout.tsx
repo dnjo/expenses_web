f932382bec5ad45c6e9a5f486f0db2fb8280f3b8
Dennis Johansson <dennis@dnjo.net>
2021-11-22 23:17:35 +0100

    Fix MUI warning

--- components/layout.tsx
+++ components/layout.tsx
@@ -42,8 +42,8 @@ export default function Layout({ children }: any) {
                             color="inherit"
                             aria-label="menu"
                             sx={{ mr: 2 }}
-                        >
-                            <MenuIcon onClick={() => setMainMenuState(true)} />
+                            onClick={() => setMainMenuState(true)}>
+                            <MenuIcon />
                         </IconButton>
                         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                             <Link href="/"><a>Expenses</a></Link>
